f29be08421ac2948723bfa86225750a1830d084d
TAEYANG YOO <yty0706@gmail.com>
2022-02-21 11:10:49 +0900

    이전코드

--- b3052/app.js
+++ b3052/app.js
@@ -20,3 +20,29 @@ function solution(arr) {
   });
   console.log(res.length);
 }
+
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs
+//   .readFileSync(filePath)
+//   .toString()
+//   .trim()
+//   .split("\n")
+//   .map((item) => Number(item));
+
+// solution(input);
+
+// function solution(arr) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(arr[i] % 42);
+//   }
+
+//   for (let i = 0; i < result.length; i++) {
+//     let same = result[i];
+//     for (let j = i; j < result.length; j++) {
+//       if (same === result[j + 1]) result.splice(j + 1);
+//     }
+//   }
+//   console.log(result.length);
+// }
