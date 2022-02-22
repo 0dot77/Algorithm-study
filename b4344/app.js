4909321313a3d88ff7942702c34ef90bf3f2b3df
TAEYANG YOO <yty0706@gmail.com>
2022-02-23 00:19:35 +0900

    b4344

--- b4344/app.js
+++ b4344/app.js
@@ -4,38 +4,40 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split(`\n`);
 const N = Number(input[0]); //5
-console.log(input);
+input.shift(); // 5 지워주기
+
+// 배열에 접근해서, 지워서 다시 배열에 넣기
 
 const newArr = [];
-for (let i = 0; i < input.length; i++) {}
+for (let i = 0; i < input.length; i++) {
+  newArr[i] = input[i].split(" ").map((item) => Number(item));
+}
 
-// console.log(typeof input);
-// let newInput = input.split(" ");
-// console.log(newInput);
+solution(newArr);
 
-// solution(input);
+function solution(arr) {
+  for (let i = 0; i < N; i++) {
+    let average = 0;
+    let studentNum = 0;
+    let studentGrade = 0;
+    let averageUp = 0;
+    let averageRatio = 0;
 
-// function solution(arr) {
-//   for (let i = 0; i < N; i++) {
-//     let average = 0;
-//     let averageRatio = 0;
-//     for (let j = 1; j < arr.length; j++) {
-//       const studentNum = arr[i][0];
-//       const studentGrade = arr[i][j];
+    //각각의 평균 구하기
+    for (let j = 1; j < arr[i].length; j++) {
+      studentNum = arr[i][0];
+      studentGrade += arr[i][j];
+    }
+    average = studentGrade / studentNum;
+    // console.log(average);
 
-//       // 학생들의 평균값 저장
-//       average += studentGrade / studentNum;
-//       console.log(studentGrade);
-//     }
-
-//     for (let k = 1; k < arr.length; k++) {
-//       // 평균값으로 평균이 넘는 학생들의 비율 구하기
-//       let averageUp = 0;
-//       if (arr[i][k] > average) {
-//         averageUp += 1;
-//       }
-//       averageRatio += averageUp / arr.length - 1;
-//     }
-//     // console.log(averageRatio);
-//   }
-// }
+    for (let k = 1; k < arr[i].length; k++) {
+      // 평균값으로 평균이 넘는 학생들의 비율 구하기
+      if (arr[i][k] > average) {
+        averageUp += 1;
+      }
+    }
+    averageRatio = (averageUp / studentNum) * 100;
+    console.log(`${averageRatio.toFixed(3)}%`);
+  }
+}
